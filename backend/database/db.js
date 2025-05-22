b041efffcf804c841d86ebdb2d602749bd87a384
lunu <nasrinlufna@gmail.com>
2025-05-22 14:30:23 +0530

    server time update mongodb db folder

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,14 @@ const mongoose=require("mongoose")
 
 exports.connectToDB=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+  socketTimeoutMS: 45000,         // 45 seconds socket timeout
+  connectTimeoutMS: 30000,        // 30 seconds initial connection timeout
+  maxPoolSize: 10,                // Limit connection pool size
+  family: 4,                      // Force IPv4 (some networks have IPv6 issues)
+});
+
         console.log('connected to DB');
     } catch (error) {
         console.log(error);
